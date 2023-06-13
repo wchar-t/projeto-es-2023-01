eb86b987f2f9591dddba6f254a7ea1f3415711b6
Joao Pedro <joaopedro99_stylle@hotmail.com>
2023-06-12 22:50:34 -0300

    register front + backend

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import '@/styles/globals.css';
 import '@/styles/theme.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import theme from '@/theme';
+import Api from '@/lib/api';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { error, result } = await Api.getSession();
+
+      Api.session = !error ? result : null;
+      setLoaded(true);
+    })();
+  }, []);
+
+  if (!loaded) {
+    return <div> </div>;
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <main className={inter.className}>
--- web/theme/index.ts
+++ web/theme/index.ts
@@ -1,8 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
+import Alert from '@/theme/alert';
+import Menu from '@/theme/menu';
 import Modal from '@/theme/modal';
 
 export default extendTheme({
   components: {
+    Alert,
+    Menu,
     Modal,
   },
 });
